2830deeac754cb6abd918337fe684957e69e095e
Yi Zhao <zhao1max@gmail.com>
2015-12-16 06:40:39 -0500

    add comment to auth and masterpage

--- test/test/Auth.cs
+++ test/test/Auth.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/**
+* Author: Yi Zhao
+*
+* Description: This page adds log out feature
+*
+*/
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,45 +13,22 @@ using System.Web;
 
 namespace test
 {
+    /// <summary>
+    /// Checks user login status, users have to login before proceed to any other pages,
+    /// and provides option to logout.
+    /// </summary>
     public class Auth
     {
+        /// <summary> Location of log in page.</summary>
         private const string LOGIN_PAGE = "LogIn_Miranda.aspx";
+
+
         private string _userName;
         private bool _isLoggedIn = false;
-        private string _currentPage = Path.GetFileName(HttpContext.Current.Request.Url.AbsolutePath);
-        
 
-
-        public Auth()
-        {
-            logIncheck();
-        }
-
-        public Auth(string name)
-        {
-            userName = name;
-            isLoggedIn = true;
-            redirect();
-        }
-
-        private void logIncheck()
-        {
-
-            if(!isLoggedIn && _currentPage != LOGIN_PAGE)
-            {
-                HttpContext.Current.Response.Redirect(LOGIN_PAGE + "?r=" + _currentPage );
-            }
-        }
-        
-        private void redirect()
-        {
-            if(HttpContext.Current.Request.QueryString["r"] != null)
-            {
-                HttpContext.Current.Response.Redirect(HttpContext.Current.Request.QueryString["r"]);
-            }
-            
-        }
-
+        /// <summary>
+        /// Stores and reads user name from session variable.
+        /// </summary>
         public string userName
         {
             get
@@ -58,6 +42,9 @@ namespace test
             }
         }
 
+        /// <summary>
+        /// Stores and reads log in status from session variable.
+        /// </summary>
         public bool isLoggedIn
         {
             get
@@ -70,7 +57,6 @@ namespace test
                 {
                     return false;
                 }
-                
             }
             set
             {
@@ -79,6 +65,59 @@ namespace test
             }
         }
 
+        /// <summary>
+        /// Stores the page before user is forced to redirected to log in page.
+        /// </summary>
+        private string _currentPage = Path.GetFileName(HttpContext.Current.Request.Url.AbsolutePath);
+        
+
+        /// <summary>
+        /// Constructor with empty parameter. It checks log in status
+        /// </summary>
+        public Auth()
+        {
+            logIncheck();
+        }
+
+        /// <summary>
+        /// Logs in the user that has been passed in, 
+        /// redirects user to the page he/she was trying to open before log in.
+        /// </summary>
+        /// <param name="name">user name that will be shown on the page </param>
+        public Auth(string name)
+        {
+            userName = name;
+            isLoggedIn = true;
+            redirect();
+        }
+
+        /// <summary>
+        /// If user is not logged in, redirects to log in page,
+        /// adds current page as query string, so it can be used to 
+        /// direct user back the page he/she tried to open.
+        /// </summary>
+        private void logIncheck()
+        {
+            if(!isLoggedIn && _currentPage != LOGIN_PAGE)
+            {
+                HttpContext.Current.Response.Redirect(LOGIN_PAGE + "?r=" + _currentPage );
+            }
+        }
+        
+        /// <summary>
+        /// Sends user back to the page before login.
+        /// </summary>
+        private void redirect()
+        {
+            if(HttpContext.Current.Request.QueryString["r"] != null)
+            {
+                HttpContext.Current.Response.Redirect(HttpContext.Current.Request.QueryString["r"]);
+            }
+        }
+        
+        /// <summary>
+        /// Logs out current user, and go to login page.
+        /// </summary>
         public void logOut()
         {
             isLoggedIn = false;
